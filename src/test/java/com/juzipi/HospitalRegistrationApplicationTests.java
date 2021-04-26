043c4803074814bfc20e87a8c74627dd1bec19d8
Xie Jun <1124685815@qq.com>
2021-04-26 21:19:00 +0800

    第一次提交

---
+++ src/test/java/com/juzipi/HospitalRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juzipi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
