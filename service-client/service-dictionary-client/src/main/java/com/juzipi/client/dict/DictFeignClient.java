4934daa8d9f92568dd0228c0ef185d3f57fe95b7
Xie Jun <1124685815@qq.com>
2021-05-08 22:30:44 +0800

    fegin远程调用模块

---
+++ service-client/service-dictionary-client/src/main/java/com/juzipi/client/dict/DictFeignClient.java
@@ -0,0 +1,23 @@
+package com.juzipi.client.dict;
+
+import com.juzipi.commonutil.tool.Result;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @Author juzipi
+ * @Date 2021/5/8 21:39
+ * @Info dict远程调用组件
+ */
+@FeignClient("service-dictionary")
+public interface DictFeignClient {
+
+
+    @GetMapping("admin/dict/getName/{dictCode}/{dictValue}")
+    public String getDictName(@PathVariable("dictCode") String dictCode, @PathVariable("dictValue") String dictValue);
+
+    @GetMapping("admin/dict/getName/{dictValue}")
+    public String getDictName(@PathVariable("dictValue") String dictValue);
+
+}
