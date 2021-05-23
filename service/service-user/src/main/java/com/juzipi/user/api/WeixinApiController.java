0f1bad16fc1861a0e556cce795a31e7cf6575c3e
Xie Jun <1124685815@qq.com>
2021-05-23 21:52:59 +0800

    微信验证码，快了，抽时间两三天就能写完了

--- service/service-user/src/main/java/com/juzipi/user/api/WeixinApiController.java
+++ service/service-user/src/main/java/com/juzipi/user/api/WeixinApiController.java
@@ -1,7 +1,16 @@
 package com.juzipi.user.api;
 
+import com.juzipi.commonutil.exception.BaseException;
+import com.juzipi.commonutil.tool.Result;
+import com.juzipi.serviceutil.core.BaseController;
+import com.juzipi.user.utils.UserProperties;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
 
 /**
  * @Author juzipi
@@ -10,11 +19,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("api/ucenter/wx")
-public class WeixinApiController {
+public class WeixinApiController extends BaseController {
 
 
     //生成二维码
+    @GetMapping("getLoginParam")
+    public Result getQrConnect(){
+        try {
+            HashMap<String, Object> hashMap = new HashMap<>();
+            hashMap.put("appid", UserProperties.WX_OPEN_APP_ID);
+            hashMap.put("scope", "snsapi_login");
+            String wxOpenRedirectUrl = UserProperties.WX_OPEN_REDIRECT_URL;
+            wxOpenRedirectUrl = URLEncoder.encode(wxOpenRedirectUrl, "utf-8");
+            hashMap.put("redirect_uri",wxOpenRedirectUrl);
+            hashMap.put("state", System.currentTimeMillis()+"");
+            return judgmentResult(hashMap);
+        } catch (UnsupportedEncodingException e) {
+            throw new BaseException(500,"生成二维码失败");
+        }
 
+    }
 
 
     //回调的方法
